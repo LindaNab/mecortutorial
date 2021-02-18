2c65ad550c98dd8f2be786cbdcefd9766eb0d86a
Linda Nab <l.nab@lumc.nl>
2021-02-18 23:41:33 +0100

    Write tutorial code

--- R/replicates.R
+++ R/replicates.R
@@ -15,104 +15,64 @@
 # Here, we'll be aiming to estimate the association between creatinine and 
 # blood pressure in pregnant women (adjusted for age)
 
-# 0. load libraries
+## 0. load libraries
 library(mecor)
+library(dplyr)
 
-# 1. load data
-data <-
-  read.csv2(file = "./McCarthy_gestationalht.csv")
+## 1. load data
+repdata <-
+  readRDS(file = "./data/sbp_creatinine.RDS")
 
-# 2. data analysis
-# Number of individuals in original data
-data %>% nrow() # 475
-# SBP_30 x Creatinine
-with(data, plot(SBP_30, Creatinine))
+## 2. data analysis
+# Show data to view structure 
+View(repdata)
+# Creatinine: serum creatinine in umol/L
+# Age: age in years
+# SBP_30: systolic blood pressure in mm Hg after 30 minutes
+# SBP_60: systolic blood pressure in mm Hg after 60 minutes
+# SBP_90: systolic blood pressure in mm Hg after 90 minutes
+# SBP_120: systolic blood pressure in mm Hg after 120 minutes
+# Systolic blood pressure is measured in all individuals after 30 minutes,
+# and measured after 60, 90 and 120 in most of the individuals
 
-# Select complete cases (of whom outcome (creatinine) and exposure (sbp_30) is
-# known)
-data <-
-  data %>%
-  select(Creatinine, Age, SBP_30, SBP_60, SBP_90, SBP_120) %>%
-  filter(!is.na(Creatinine) & !is.na(SBP_30))
-data %>% nrow() # 451 (24 cases filtered)
-# Correct typo
-data[which(data$SBP_90 == 1354),]$SBP_90 <- 135
-# Delete outlier in creatinine from data
-data <-
-  data %>%
-  filter(Creatinine > 10)
-data %>% nrow() # 450
+# Plot first systolic blood pressure vs creatinine 
+with(repdata, 
+     plot(SBP_30, Creatinine, 
+          xlab = "Systolic blood pressure (mm Hg)",
+          ylab = "Creatinine (umol/L)"))
 
-# Create balanced replicates study (select only individuals of whom 3 replicate
-# measures are available)
-cc <-
-  which(!is.na(data$SBP_60) & !is.na(data$SBP_90) & !is.na(data$SBP_120))
-data$SBP_60[-cc] <- NA
-data$SBP_90[-cc] <- NA
-data$SBP_120[-cc] <- NA
-
-# Linear fit using only the first BP measurement:
+# Fit a linear model using the first BP measurement:
 naivefit <-
   lm(Creatinine ~ SBP_30 + Age,
-     data = data,
-     x = TRUE)
-naivefit
-confint(naivefit)
+     data = repdata)
+naivefit %>% summary()
 
-# Correcting for measurement error using the 3 replicates using regcal:
+# Correcting for measurement error using the 3 replicates applying regcal:
 mefit <-
   mecor(Creatinine ~ MeasError(SBP_30, replicate =
                                  cbind(SBP_60, SBP_90, SBP_120)) + Age,
-        data = data)
-mefit
+        data = repdata,
+        method = "standard")
 mefit %>% summary()
+
+# Correcting for measurement error using the 3 replicates applying mle:
 mefit_mle <-
   mecor(Creatinine ~ MeasError(SBP_30, replicate =
                                  cbind(SBP_60, SBP_90, SBP_120)) + Age,
-        data = data,
+        data = repdata,
         method = "mle")
-mefit_mle # different from reg cal
+mefit_mle %>% summary() # different from reg cal
 
-# Calculate variance in bp measurements:
-sbp_var <- apply(data %>%
-                   select(SBP_30, SBP_60, SBP_90, SBP_120), 1, var,
-                 use = "pairwise.complete.obs") %>%
+# Calculate within individuals variance in bp measurements:
+sbp_var <-
+  apply(repdata %>% select(SBP_30, SBP_60, SBP_90, SBP_120),
+        1, 
+        var,
+        use = "pairwise.complete.obs") %>%
   mean(na.rm = TRUE)
 
 # Correcting for measurement error using the estimated variance
 mefit_random <-
   mecor(Creatinine ~ MeasErrorRandom(SBP_30, var = sbp_var) + Age,
-        data = data)
-mefit_random
-
-mefit_simex <-
-  simex(naivefit,
-        SIMEXvariable = "SBP_30",
-        measurement.error = sqrt(sbp_var),
-        asymptotic = TRUE)
-mefit_simex
-mefit_simex$coefficients[2] + qnorm(0.025) *
-  summary(mefit_simex)$coefficients$asymptotic[2,2]
-mefit_simex$coefficients[2] - qnorm(0.025) *
-  summary(mefit_simex)$coefficients$asymptotic[2,2]
-
-png("./output/pregnantwomen.png")
-with(data, plot(SBP_30, Creatinine,
-                pch = 20,
-                xlab = "Systolic blood pressure (mm Hg, first measurement)",
-                ylab = "Serum Creatinine (umol/L)",
-                main = "Pregnant women",
-                col = "gray"))
-abline(a = naivefit$coefficients[1],
-       b = naivefit$coefficients[2], col = "black",
-       lwd = 2)
-abline(a = mefit$corfit$coef[1],
-       b = mefit$corfit$coef[2], col = "red",
-       lwd = 2,
-       lty = 2)
-legend("topright", c("Uncorrected", "Corrected"),
-       lwd = 2,
-       lty = c(1, 2),
-       col = c("black", "red"))
-dev.off()
-
+        data = repdata)
+mefit_random %>% summary()
