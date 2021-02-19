47eb6ec1efcbdd7e9450caab598d1090e5726c09
Linda Nab <l.nab@lumc.nl>
2021-02-20 00:04:18 +0100

    Add script to load calibration data

---
+++ R/data_loadcaldata.R
@@ -0,0 +1,15 @@
+#==================================#
+# mecor tutorial                   #
+# Calibration study                #
+# preprocess data                  #
+#                                  #
+# 20210218 lindanab4@gmail.com     #
+#==================================#
+
+# Data: https://datadryad.org/stash/dataset/doi:10.5061/dryad.ck501
+# Paper: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0189254
+# Original rq: Irisin, physical activity and fitness status in healthy humans: 
+# No association under resting conditions in a cross-sectional study
+
+# Here, we'll be aiming to estimate the association between ....
+
